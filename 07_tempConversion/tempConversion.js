de8bd86b5dde90486145a368e386bbb80b43ad41
Johnnyn340 <114721267+Johnnyn340@users.noreply.github.com>
2022-12-26 18:04:31 -0800

    Finished js exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToFahrenheit = function(cNum) {
+  sum = (cNum * 1.8) + 32;
+  const result = Math.round(sum * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(fNum) {
+  sum = (fNum -32) *.5556;
+  const result = Math.round(sum * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
