342cfab075d1ec81934c4a9cbca824053ab26a22
Johnnyn340 <114721267+Johnnyn340@users.noreply.github.com>
2023-01-31 14:27:53 -0800

    completed palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let len = cleanedStr.length;
+    for (let i = 0; i < len/2; i++) {
+        if (cleanedStr[i] !== cleanedStr[len - 1 - i]) {
+            return false;
+        }
+  }
+    return true;
 };
 
 // Do not edit below this line
